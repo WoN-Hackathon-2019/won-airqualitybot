e8b07577c4a99b1fb546d81ecdebc7279c00d2f8
leaze <z.leonharda@gmail.com>
2019-12-10 12:24:06 +0100

    add dtos;

---
+++ src/main/java/won/bot/skeleton/dto/LocationMeasurements.java
@@ -0,0 +1,14 @@
+package won.bot.skeleton.dto;
+
+import java.util.List;
+
+public class LocationMeasurements {
+
+    private String location;
+
+    private String city;
+
+    private String country;
+
+    private List<Measurement> measurements;
+}
---
+++ src/main/java/won/bot/skeleton/dto/Measurement.java
@@ -0,0 +1,10 @@
+package won.bot.skeleton.dto;
+
+public class Measurement {
+
+    private String parameter;
+
+    private Double value;
+
+    private String unit;
+}
