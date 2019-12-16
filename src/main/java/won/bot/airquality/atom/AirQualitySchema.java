c70c2061d3d0b23f7bcb25bfcba1b85604f52221
leaze <z.leonharda@gmail.com>
2019-12-16 11:18:21 +0100

    use actual schema;

--- src/main/java/won/bot/airquality/atom/AirQualitySchema.java
+++ src/main/java/won/bot/airquality/atom/AirQualitySchema.java
@@ -6,16 +6,25 @@ import org.apache.jena.rdf.model.Property;
 
 public class AirQualitySchema {
 
+    private AirQualitySchema() {
+    }
+
     private static Model m = ModelFactory.createDefaultModel();
 
-    public static final Property LOCATION = m.createProperty("http://schema.org/location");
-    public static final Property CITY = m.createProperty("http://schema.org/city");
-    public static final Property COUNTRY = m.createProperty("http://schema.org/country");
-    public static final Property MEASUREMENTS = m.createProperty("http://schema.org/measurements"); // TODO use actual URIs?
+    public static final Property LOCATION = m.createProperty("http://schema.org/Place");
 
-    public static final Property MEASUREMENT = m.createProperty("http://schema.org/measurement");
-    public static final Property MEASURE_PARAM = m.createProperty("http://schema.org/measure_param");
-    public static final Property MEASURE_PARAM_NAME = m.createProperty("http://schema.org/measure_param_name");
-    public static final Property MEASURE_VALUE = m.createProperty("http://schema.org/measure_value");
-    public static final Property MEASURE_UNIT = m.createProperty("http://schema.org/measure_unit");
+    public static final Property LOC_ADDRESS = m.createProperty("http://schema.org/address");
+    public static final Property ADDRESS = m.createProperty("http://schema.org/PostalAddress");
+    public static final Property COUNTRY = m.createProperty("http://schema.org/addressCountry");
+    public static final Property CITY = m.createProperty("http://schema.org/addressRegion");
+    public static final Property LOCALITY = m.createProperty("http://schema.org/addressLocality");
+
+    public static final Property LOC_MEASUREMENT = m.createProperty("http://schema.org/amenityFeature");
+    public static final Property MEASUREMENT = m.createProperty("http://schema.org/LocationFeatureSpecification");
+    public static final Property MEASURE_DATE = m.createProperty("http://schema.org/validFrom");
+    public static final Property MEASURE_PARAM = m.createProperty("http://schema.org/propertyID");
+    public static final Property MEASURE_PARAM_NAME = m.createProperty("http://schema.org/name");
+    public static final Property MEASURE_VALUE = m.createProperty("http://schema.org/value");
+    public static final Property MEASURE_UNIT = m.createProperty("http://schema.org/unitCode");
+    public static final Property MEASURE_UNIT_TEXT = m.createProperty("http://schema.org/unitText");
 }
--- src/main/java/won/bot/airquality/atom/AtomFactory.java
+++ src/main/java/won/bot/airquality/atom/AtomFactory.java
@@ -1,6 +1,7 @@
 package won.bot.airquality.atom;
 
 import org.apache.jena.query.Dataset;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Resource;
 import won.bot.airquality.dto.LocationMeasurements;
 import won.protocol.util.DefaultAtomModelWrapper;
@@ -23,14 +24,18 @@ public class AtomFactory {
         atomWrapper.addTag(AQ_DATA_TAG);
         atomWrapper.addTag(AQ_BOT_TAG);
 
-        Resource locationNode = atomWrapper.getAtomContentNode();  // create a blank node that represents a locationNode
-        locationNode.addProperty(AirQualitySchema.LOCATION, locationMeasurements.getLocation());
-        locationNode.addProperty(AirQualitySchema.CITY, locationMeasurements.getCity());
-        locationNode.addProperty(AirQualitySchema.COUNTRY, locationMeasurements.getCountry());
+        Model model = atomWrapper.getAtomModel();
+        Resource locationNode = model.createResource(AirQualitySchema.LOCATION);
+
+        Resource addressNode = atomWrapper.getAtomModel().createResource(AirQualitySchema.ADDRESS);
+        addressNode.addProperty(AirQualitySchema.COUNTRY, locationMeasurements.getCountry());
+        addressNode.addProperty(AirQualitySchema.CITY, locationMeasurements.getCity());
+        addressNode.addProperty(AirQualitySchema.LOCALITY, locationMeasurements.getLocation());
+        locationNode.addProperty(AirQualitySchema.LOC_ADDRESS, addressNode);
 
         locationMeasurements.getMeasurements().stream()
                 .map(measurement -> {
-                    Resource measurementNode = locationNode.getModel().createResource(); // create the second blank node (which represents a measurementNode)
+                    Resource measurementNode = locationNode.getModel().createResource(AirQualitySchema.MEASUREMENT);
                     measurementNode.addProperty(AirQualitySchema.MEASURE_PARAM, measurement.getParameter());
                     measurementNode.addProperty(AirQualitySchema.MEASURE_VALUE, String.valueOf(measurement.getValue()));
                     measurementNode.addProperty(AirQualitySchema.MEASURE_UNIT, String.valueOf(measurement.getUnit()));
@@ -44,7 +49,7 @@ public class AtomFactory {
                     // TODO maybe add name of parameter (not just ID)
                     return measurementNode;
                 })
-                .forEach(measurementResource -> locationNode.addProperty(AirQualitySchema.MEASUREMENT, measurementResource));
+                .forEach(measurementResource -> locationNode.addProperty(AirQualitySchema.LOC_MEASUREMENT, measurementResource));
 
         return atomWrapper.getDataset();
     }
